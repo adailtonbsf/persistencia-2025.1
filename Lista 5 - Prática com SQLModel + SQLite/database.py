1d1259f2b6980c09b1167aaa108ae92bd31b5621
Adailton Bezerra <70333667+adailtonbsf@users.noreply.github.com>
2025-05-20 11:18:47 -0300

    add: lista 5

---
+++ Lista 5 - Prática com SQLModel + SQLite/database.py
@@ -0,0 +1,14 @@
+from sqlmodel import SQLModel, create_engine, Session
+from dotenv import load_dotenv
+import os
+
+load_dotenv("db.env")
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL, echo=True)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    return Session(engine)
